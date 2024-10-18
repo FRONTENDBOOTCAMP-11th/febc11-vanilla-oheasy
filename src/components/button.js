dda9c76a3556c9bd5679967578d4e57157828524
Donghwan Oh <imodh97@gmail.com>
2024-10-18 17:02:07 +0900

    ✨ Feat: 버튼 컴포넌트 추가 (DH)

---
+++ src/components/button.js
@@ -0,0 +1,16 @@
+import { loadHTML } from '../utils/loadHTML';
+
+// html 컴포넌트 파일을 불러와 콜백함수를 실행
+loadHTML('/src/components/button.html', function (response) {
+  const $buttonBox = document.getElementById('button-box');
+
+  $buttonBox.innerHTML = response;
+  const $button = $buttonBox.children[2];
+
+  $button.textContent = $buttonBox.dataset.text;
+  $button.style.backgroundColor = $buttonBox.dataset.bgcolor;
+  $button.style.color = $buttonBox.dataset.color;
+  $button.style.width = $buttonBox.dataset.width;
+  $button.style.height = $buttonBox.dataset.height;
+  $button.style.lineHeight = $buttonBox.dataset.height;
+});
--- src/components/header-mobile.js
+++ src/components/header-mobile.js
@@ -2,5 +2,5 @@ import { loadHTML } from '../utils/loadHTML';
 
 // html 컴포넌트 파일을 불러와 콜백함수를 실행
 loadHTML('/src/components/header-mobile.html', function (response) {
-  document.getElementById('header').innerHTML = response;
+  document.getElementById('header-box').innerHTML = response;
 });
