737b9559c81e966e81bb8ec6cc1d954f9d19a343
Donghwan Oh <imodh97@gmail.com>
2024-10-19 20:04:32 +0900

    ✨ Feat: 버튼 컴포넌트에 fontweight 설정 추가 (DH)

--- src/components/button.js
+++ src/components/button.js
@@ -8,7 +8,7 @@ loadHTML('/src/components/button.html', function (response) {
     const contents = buttonBox.innerHTML; // 작성된 inner html을 저장
 
     buttonBox.innerHTML = response; // button box의 inner html을 button.html로 교체
-    const $button = buttonBox.children[2]; // button box 자식 중 button을 선택
+    const $button = buttonBox.querySelector('.btn'); // button box 자식 중 button을 선택
 
     /* button 스타일 수정 */
     $button.innerHTML = contents; // 저장된 inner html 되돌리기
@@ -26,5 +26,8 @@ loadHTML('/src/components/button.html', function (response) {
     // size
     $button.style.width = buttonBox.dataset.width;
     $button.style.height = buttonBox.dataset.height;
+
+    if (buttonBox.dataset.fontweight)
+      $button.style.fontWeight = +buttonBox.dataset.fontweight;
   });
 });
