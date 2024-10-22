b35194e1e1889c2430feaa4e1d8e056d23ea08f9
jiyoungnim <cloundyon31@gmail.com>
2024-10-22 11:41:56 +0900

    💄 footer-mobile.js 디버깅용 컨솔로그 코드 삭제 (JY) resolves: develop

--- src/components/footer-mobile.js
+++ src/components/footer-mobile.js
@@ -1,7 +1,5 @@
 import { loadHTML } from '../utils/loadHTML.js';
 
 loadHTML('/src/components/footer-mobile.html', function (response) {
-  console.log(response);
-
   document.getElementById('footer-wrapper').innerHTML = response;
 });
