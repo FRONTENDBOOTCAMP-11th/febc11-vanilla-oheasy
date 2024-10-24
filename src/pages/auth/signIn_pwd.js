23bb8cc0d1b4efc229c9b255eeba715b7e8815c1
yeon1ee <carpediem.y13@gmail.com>
2024-10-24 14:56:23 +0900

    🔧conf: prettier 적용

--- src/pages/auth/signIn_pwd.js
+++ src/pages/auth/signIn_pwd.js
@@ -12,91 +12,88 @@ const $btnBox = document.querySelector('.signIn-pwd-btn');
 const userEmail = sessionStorage.getItem('email');
 
 //이메일 전달받기
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
+  console.log(userEmail);
 
-    console.log(userEmail);
-
-    //전달 받은 이메일이 있다면 출력
-    if (userEmail) {
-        $received_email.textContent = userEmail;
-    } else {
-        //전달 받은 이메일이 없다면 출력
-        $received_email.textContent = '전달받은 이메일이 없습니다.';
-    }
+  //전달 받은 이메일이 있다면 출력
+  if (userEmail) {
+    $received_email.textContent = userEmail;
+  } else {
+    //전달 받은 이메일이 없다면 출력
+    $received_email.textContent = '전달받은 이메일이 없습니다.';
+  }
 });
 
 //이전 버튼을 눌렀을 때 -> signIn_main (기존) or signUp_main (신규)
 $previousBtn.addEventListener('click', function () {
-    //이전 페이지 url을 가져온다. (.reffer)
-    const previousPage = document.referrer;
+  //이전 페이지 url을 가져온다. (.reffer)
+  const previousPage = document.referrer;
 
-    if(previousPage.includes('signIn_main.html')) {
-        window.location.href = 'signIn_main.html';
-    } else if (previousPage.includes('signUp_main.html')) {
-        window.location.href = 'signUp_main.html';
-    }
+  if (previousPage.includes('signIn_main.html')) {
+    window.location.href = 'signIn_main.html';
+  } else if (previousPage.includes('signUp_main.html')) {
+    window.location.href = 'signUp_main.html';
+  }
 });
 
 //로그인 버튼을 눌렀을 때
-$loginBtn.addEventListener('click', async function(event) {
-    event.preventDefault();
+$loginBtn.addEventListener('click', async function (event) {
+  event.preventDefault();
 
-    const pwdInput = $userPwd.value;
+  const pwdInput = $userPwd.value;
 
-    //비밀번호를 입력 받지 못했을 때
-    if (!pwdInput) {
-        $result.textContent = '비밀번호를 입력해주세요.';
+  //비밀번호를 입력 받지 못했을 때
+  if (!pwdInput) {
+    $result.textContent = '비밀번호를 입력해주세요.';
+    $result.style.color = 'red';
+
+    return;
+  }
+
+  //입력 받았을 때
+  const getPwd = async function (userEmail, userPwd) {
+    try {
+      const response = await axios.post(
+        'https://11.fesp.shop/users/login',
+        {
+          email: userEmail,
+          password: userPwd,
+        },
+        {
+          headers: {
+            'client-id': 'vanilla05',
+          },
+        },
+      );
+
+      //로그인에 성공
+      if (response.data.ok === 1) {
+        //토큰을 받아오고 저장한다.
+        const accessToken = response.data.item.token.accessToken;
+        const refreshToken = response.data.item.token.refreshToken;
+
+        sessionStorage.setItem('accessToken', accessToken);
+        sessionStorage.setItem('refreshToken', refreshToken);
+
+        //home으로 이동한다.
+        window.location.href = '/index.html';
+      } else {
+        //로그인 실패
+        $result.textContent = '비밀번호가 일치하지 않습니다.';
         $result.style.color = 'red';
-
-        return;
+      }
+    } catch (error) {
+      //422 에러처리
+      if (error.response && error.response.status === 422) {
+        $result.textContent = '비밀번호가 일치하지 않습니다.';
+        $result.style.color = 'red';
+      } else {
+        console.log('서버에서 에러가 발생하였습니다.', error);
+        $result.textContent = '서버에서 오류가 발생했습니다.';
+        $result.style.color = 'red';
+      }
     }
+  };
 
-    //입력 받았을 때
-    const getPwd = async function (userEmail, userPwd) {
-        try {
-            const response = await axios.post('https://11.fesp.shop/users/login', {
-                email: userEmail,
-                password: userPwd,
-            }, { 
-                headers: {
-                    'client-id': 'vanilla05'
-                }
-            });
-
-            //로그인에 성공
-            if (response.data.ok === 1) {
-                //토큰을 받아오고 저장한다.
-                const accessToken = response.data.item.token.accessToken;
-                const refreshToken = response.data.item.token.refreshToken;
-
-                sessionStorage.setItem('accessToken', accessToken);
-                sessionStorage.setItem('refreshToken', refreshToken);
-
-                //home으로 이동한다.
-                window.location.href = '/index.html';
-            } else {
-                //로그인 실패
-                $result.textContent = '비밀번호가 일치하지 않습니다.';
-                $result.style.color = 'red';
-            }
-        }
-
-        //422 에러처리
-        catch (error) {
-            if (error.response && error.response.status === 422) {
-                $result.textContent = '비밀번호가 일치하지 않습니다.';
-                $result.style.color = 'red';
-            } 
-            else {
-                console.log('서버에서 에러가 발생하였습니다.', error);
-                $result.textContent = '서버에서 오류가 발생했습니다.';
-                $result.style.color = 'red';
-            }
-        }
-    
-    }
-
-    getPwd(userEmail, pwdInput);
-
-})
-
+  getPwd(userEmail, pwdInput);
+});
