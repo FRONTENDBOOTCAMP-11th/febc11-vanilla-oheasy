547209fa88381c3fe7917dcad5a83c00c8099048
Donghwan Oh <imodh97@gmail.com>
2024-10-28 22:10:43 +0900

    ♻️ 코드 리팩토링, 쿼리 스트링에서 product id 불러오기, 장바구니에 추가 시도중... (DH)

--- src/pages/details/details.js
+++ src/pages/details/details.js
@@ -1,162 +1,5 @@
 import axios from 'axios';
 
-const productId = 1;
-
-window.addEventListener('load', async function () {
-  // item-info
-  const $name = document.querySelector('.item-info .name');
-  const $category = document.querySelector('.item-info .category');
-  const $onSale = document.querySelector('.price .on-sale');
-  const $original = document.querySelector('.price .original');
-  const $discount = document.querySelector('.price .discount');
-
-  const product = await getProduct(productId);
-  console.log(product);
-
-  const category1 = await getCategory(product, 1);
-  const category2 = await getCategory(
-    product,
-    2,
-    product.item.extra.category[0],
-  );
-
-  $name.textContent = product.item.name;
-  $category.textContent = `${category1} ${category2}`;
-  $onSale.textContent = formatPrice(product.item.price);
-
-  if (product.item.extra.primeCost !== product.item.price) {
-    $original.textContent = formatPrice(product.item.extra.primeCost);
-    $discount.textContent = getDiscountRate(
-      product.item.extra.primeCost,
-      product.item.price,
-    );
-  }
-
-  // item-cover & item-size
-  // thumbnail이 클릭되었을 때 바뀌어야 하는 요소
-  const $coverThumbnails = document.querySelector('.item-cover-thumbnails');
-
-  product.item.options.map(
-    e =>
-      ($coverThumbnails.innerHTML += `<img class='thumbnail' src='https://11.fesp.shop/files/vanilla05/${e.mainImages[0].name}' />`),
-  );
-
-  const currentOption = { option: 0, size: null };
-  // let currentOption = 0;
-  // let size = null;
-  renderImage(product, currentOption);
-  renderSize(product, currentOption);
-  renderDesc(product, currentOption);
-
-  const thumbnails = [...$coverThumbnails.querySelectorAll('.thumbnail')];
-  thumbnails.forEach((e, i) => {
-    e.addEventListener('click', function (e) {
-      thumbnails.forEach(e => {
-        e.classList.remove('clicked');
-      });
-
-      currentOption.option = i;
-      currentOption.size = null;
-      renderImage(product, currentOption);
-      e.target.classList.add('clicked');
-
-      renderSize(product, currentOption);
-      renderDesc(product, currentOption);
-    });
-  });
-
-  // item-btn
-  const $bagBtn = document.querySelector(
-    '.item-buttons .button-box:first-child button',
-  );
-  $bagBtn.addEventListener('click', function () {
-    if (currentOption.size === null) {
-      window.alert('사이즈를 선택해 주세요.');
-    } else
-      console.log(
-        `productId: ${productId}, 옵션: ${currentOption.option}, 사이즈: ${currentOption.size}`,
-      );
-  });
-
-  // description
-});
-
-const renderImage = function (product, currentOption) {
-  const $coverMain = document.querySelector('.item-cover-main');
-  console.log(currentOption);
-  $coverMain.innerHTML = '';
-  if (product.item.options.length === 0) {
-    product.item.mainImages.map(
-      e =>
-        ($coverMain.innerHTML += `<img src='https://11.fesp.shop/files/vanilla05/${e.name}
-      ' />`),
-    );
-  } else {
-    product.item.options[currentOption.option].mainImages.map(
-      e =>
-        ($coverMain.innerHTML += `<img src='https://11.fesp.shop/files/vanilla05/${e.name}
-' />`),
-    );
-  }
-};
-
-// 상품의 사이즈 목록을 렌더한 후
-// 사이즈 클릭 시 해당 값을 출력하게 하는 함수
-const renderSize = function (product, currentOption) {
-  const $grid = document.querySelector('.size-grid');
-  $grid.innerHTML = '';
-
-  // 옵션이 하나밖에 없을 때
-  if (product.item.options.length === 0) {
-    product.item.extra.size.map(e => {
-      $grid.innerHTML += `<span>${e}</span>`;
-    });
-  } else {
-    product.item.options[currentOption.option].extra.size.map(e => {
-      $grid.innerHTML += `<span>${e}</span>`;
-    });
-  }
-
-  const sizes = [...$grid.querySelectorAll('span')];
-  sizes.forEach(e => {
-    e.addEventListener('click', function (e) {
-      currentOption.size = e.target.textContent;
-
-      [...$grid.querySelectorAll('span')].map(e => {
-        e.classList.remove('size-clicked');
-      });
-      e.target.classList.add('size-clicked');
-    });
-  });
-};
-
-const renderDesc = function (product, currentOption) {
-  const $description = document.querySelector('.description');
-  $description.querySelector('p').textContent = product.item.content;
-  $description.querySelector('.list li:first-child').textContent =
-    product.item.options[currentOption.option].extra.color;
-  $description.querySelector('.list li:nth-child(2)').textContent =
-    `스타일 번호: 
-    ${product.item.options[currentOption.option].extra.styleNo}`;
-};
-// 할인률을 계산해 문자열을 리턴하는 함수
-const getDiscountRate = function (original, onSale) {
-  return Math.trunc(((original - onSale) / original) * 100) + '% 할인';
-};
-
-// 가격에 콤마와 원을 붙인 문자열을 리턴하는 함수
-const formatPrice = function (price) {
-  const arr = String(price).split('');
-  let count = 0;
-  for (let i = arr.length; i >= 0; i--) {
-    if (i !== 0 && count !== 0 && count % 3 === 0) {
-      arr[i] = ',' + arr[i];
-    }
-    count++;
-  }
-  return arr.join('') + '원';
-};
-
 // 상품 객체를 리턴하는 함수
 const getProduct = async function (productId) {
   try {
@@ -182,7 +25,7 @@ const getCategory = async function (product, depth, parent = null) {
   console.log(depth, parent);
   try {
     const response = await axios.get(
-      `https://11.fesp.shop//codes/productCategory?depth=${depth}${parent ? '&parent=' + parent : ''}`,
+      `https://11.fesp.shop/codes/productCategory?depth=${depth}${parent ? '&parent=' + parent : ''}`,
       {
         headers: {
           'client-id': 'vanilla05',
@@ -215,3 +58,181 @@ const getCategory = async function (product, depth, parent = null) {
     return null;
   }
 };
+
+const renderImage = function () {
+  const $coverMain = document.querySelector('.item-cover-main');
+  $coverMain.innerHTML = '';
+  if (product.item.options.length === 0) {
+    product.item.mainImages.map(
+      e =>
+        ($coverMain.innerHTML += `<img src='https://11.fesp.shop/files/vanilla05/${e.name}
+      ' />`),
+    );
+  } else {
+    product.item.options[currentOption.option].mainImages.map(
+      e =>
+        ($coverMain.innerHTML += `<img src='https://11.fesp.shop/files/vanilla05/${e.name}
+' />`),
+    );
+  }
+};
+
+// 상품의 사이즈 목록을 렌더한 후
+// 사이즈 클릭 시 해당 값을 출력하게 하는 함수
+const renderSize = function () {
+  const $grid = document.querySelector('.size-grid');
+  $grid.innerHTML = '';
+
+  // 옵션이 하나밖에 없을 때
+  if (product.item.options.length === 0) {
+    product.item.extra.size.map(e => {
+      $grid.innerHTML += `<span>${e}</span>`;
+    });
+  } else {
+    product.item.options[currentOption.option].extra.size.map(e => {
+      $grid.innerHTML += `<span>${e}</span>`;
+    });
+  }
+
+  const sizes = [...$grid.querySelectorAll('span')];
+  sizes.forEach(e => {
+    e.addEventListener('click', function (e) {
+      currentOption.size = e.target.textContent;
+
+      [...$grid.querySelectorAll('span')].map(e => {
+        e.classList.remove('size-clicked');
+      });
+      e.target.classList.add('size-clicked');
+    });
+  });
+};
+
+const renderDesc = function () {
+  const $description = document.querySelector('.description');
+  $description.querySelector('p').textContent = product.item.content;
+  $description.querySelector('.list li:first-child').textContent =
+    product.item.options.length > 0
+      ? product.item.options[currentOption.option].extra.color
+      : product.item.extra.color;
+  $description.querySelector('.list li:nth-child(2)').textContent =
+    product.item.options.length > 0
+      ? `스타일 번호: 
+    ${product.item.options[currentOption.option].extra.styleNo}`
+      : `스타일 번호: 
+    ${product.item.extra.styleNo}`;
+};
+// 할인률을 계산해 문자열을 리턴하는 함수
+const getDiscountRate = function (original, onSale) {
+  return Math.trunc(((original - onSale) / original) * 100) + '% 할인';
+};
+
+// 가격에 콤마와 원을 붙인 문자열을 리턴하는 함수
+const formatPrice = function (price) {
+  const arr = String(price).split('');
+  let count = 0;
+  for (let i = arr.length; i >= 0; i--) {
+    if (i !== 0 && count !== 0 && count % 3 === 0) {
+      arr[i] = ',' + arr[i];
+    }
+    count++;
+  }
+  return arr.join('') + '원';
+};
+
+const urlSearch = new URLSearchParams(location.search);
+const productId = urlSearch.get('productId');
+
+// item-info
+const $name = document.querySelector('.item-info .name');
+const $category = document.querySelector('.item-info .category');
+const $onSale = document.querySelector('.price .on-sale');
+const $original = document.querySelector('.price .original');
+const $discount = document.querySelector('.price .discount');
+
+const product = await getProduct(productId);
+console.log(product);
+
+const category1 = await getCategory(product, 1);
+const category2 = await getCategory(product, 2, product.item.extra.category[0]);
+
+$name.textContent = product.item.name;
+$category.textContent = `${category1} ${category2}`;
+$onSale.textContent = formatPrice(product.item.price);
+
+// 원가와 할인가가 다를때만 할인율 표시
+if (product.item.extra.primeCost !== product.item.price) {
+  $original.textContent = formatPrice(product.item.extra.primeCost);
+  $discount.textContent = getDiscountRate(
+    product.item.extra.primeCost,
+    product.item.price,
+  );
+}
+
+// item-cover & item-size
+// thumbnail이 클릭되었을 때 바뀌어야 하는 요소
+const $coverThumbnails = document.querySelector('.item-cover-thumbnails');
+
+product.item.options.map(
+  e =>
+    ($coverThumbnails.innerHTML += `<img class='thumbnail' src='https://11.fesp.shop/files/vanilla05/${e.mainImages[0].name}' />`),
+);
+
+// 옵션 객체
+const currentOption = { option: 0, size: null };
+
+renderImage(product, currentOption);
+renderSize(product, currentOption);
+renderDesc(product, currentOption);
+
+const thumbnails = [...$coverThumbnails.querySelectorAll('.thumbnail')];
+thumbnails.forEach((e, i) => {
+  e.addEventListener('click', function (e) {
+    thumbnails.forEach(e => {
+      e.classList.remove('clicked');
+    });
+
+    currentOption.option = i;
+    currentOption.size = null;
+    renderImage(product, currentOption);
+    e.target.classList.add('clicked');
+
+    renderSize(product, currentOption);
+    renderDesc(product, currentOption);
+  });
+});
+
+// item-btn
+const $bagBtn = document.querySelector(
+  '.item-buttons .button-box:first-child button',
+);
+$bagBtn.addEventListener('click', async function () {
+  if (currentOption.size === null) {
+    window.alert('사이즈를 선택해 주세요.');
+  } else {
+    console.log(
+      `productId: ${productId}, 옵션: ${currentOption.option}, 사이즈: ${currentOption.size}`,
+    );
+
+    try {
+      const response = await axios.post(
+        'https://11.fesp.shop/carts',
+        {
+          product_id: productId,
+          quantity: 1,
+          option: currentOption.option,
+          size: currentOption.size,
+        },
+        {
+          headers: {
+            'client-id': 'vanilla05',
+            Authorization:
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjQsInR5cGUiOiJ1c2VyIiwibmFtZSI6IuygnOydtOyngCIsImVtYWlsIjoidTFAZ21haWwuY29tIiwiaW1hZ2UiOiIvZmlsZXMvdmFuaWxsYTA1L3VzZXItamF5Zy53ZWJwIiwibG9naW5UeXBlIjoiZW1haWwiLCJpYXQiOjE3MzAwNzQ0ODMsImV4cCI6MTczMDE2MDg4MywiaXNzIjoiRkVTUCJ9.-WFqisZ0vAbwaWyryapt5zAnME_EiznoF5-8lCSyeek',
+          },
+        },
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
