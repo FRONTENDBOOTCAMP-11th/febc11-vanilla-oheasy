db388611497fd238ceb744942318a37830e498d6
Donghwan Oh <imodh97@gmail.com>
2024-10-31 13:35:25 +0900

    🔧 vite.config.js파일 경로 수정, Config: top-level-await 패키지 설치 (DH)

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,15 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: resolve(__dirname, 'index.html'), // 메인 홈
-        signIn_main: resolve(__dirname, 'src/pages/auth/signIn_main.html'), // 로그인
-        signIn_pwd: resolve(__dirname, 'src/pages/auth/signIn_pwd.html'), // 로그인
+        signIn: resolve(__dirname, 'src/pages/auth/signIn_main.html'), // 로그인
         signUp_main: resolve(__dirname, 'src/pages/auth/signUp_main.html'), // 회원가입
         signUp_terms: resolve(__dirname, 'src/pages/auth/signUp_terms.html'), // 회원가입
         bag: resolve(__dirname, 'src/pages/bag/bag.html'), // 장바구니
         products: resolve(__dirname, 'src/pages/products/products.html'), // 상품 리스트
         details: resolve(__dirname, 'src/pages/details/details.html'), // 상품 디테일
         button: resolve(__dirname, 'src/components/button.html'), // 버튼 컴포넌트
-        footer: resolve(__dirname, 'src/components/footer-mobile.html'), // footer 컴포넌트
-        header: resolve(__dirname, 'src/components/header-mobile.html'), // header 컴포넌트
-
+        footer: resolve(__dirname, 'src/components/footer.html'), // footer 컴포넌트
+        header: resolve(__dirname, 'src/components/header.html'), // header 컴포넌트
         // 필요한 다른 HTML 파일을 여기에 추가
       },
     },
