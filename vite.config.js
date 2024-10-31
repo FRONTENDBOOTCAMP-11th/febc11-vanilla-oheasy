de1195390a4fbb59fd8a1be430ac15ba2c91a767
Donghwan Oh <imodh97@gmail.com>
2024-10-31 16:27:15 +0900

    🔧 Config: vite.config.js 경로 수정 (DH)

--- vite.config.js
+++ vite.config.js
@@ -14,8 +14,8 @@ export default defineConfig({
         products: resolve(__dirname, 'src/pages/products/products.html'), // 상품 리스트
         details: resolve(__dirname, 'src/pages/details/details.html'), // 상품 디테일
         button: resolve(__dirname, 'src/components/button.html'), // 버튼 컴포넌트
-        footer: resolve(__dirname, 'src/components/footer-mobile.html'), // footer 컴포넌트
-        header: resolve(__dirname, 'src/components/header-mobile.html'), // header 컴포넌트
+        footer: resolve(__dirname, 'src/components/footer.html'), // footer 컴포넌트
+        header: resolve(__dirname, 'src/components/header.html'), // header 컴포넌트
 
         // 필요한 다른 HTML 파일을 여기에 추가
       },
