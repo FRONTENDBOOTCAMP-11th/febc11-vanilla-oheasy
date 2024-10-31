dc0047109740f23ea37703e6308e2a1739edfe65
Donghwan Oh <imodh97@gmail.com>
2024-10-31 13:40:30 +0900

    🔧 Config: top-await 패키지 제거, vite.config 재 설정 (DH)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  esbuild: {
+    supported: {
+      'top-level-await': true, //browsers can handle top-level-await features
+    },
+  },
   build: {
     rollupOptions: {
       input: {
